8fc34e1fa35516b4f821a4e0dd026293e37b71ce
Alex Nisnevich <alex.nisnevich@gmail.com>
2015-11-13 01:17:49 -0800

    spot o cleanup

--- lambda_terms.rb
+++ lambda_terms.rb
@@ -13,12 +13,9 @@ class String
 end
 
 def main
-  patterns = find_patterns(15)
+  patterns = find_patterns(10)
 
-  combinators = {}
-  patterns.each_pair {|k, v| 
-    combinators[k] = v.map {|p| combinators_by_pattern(p) }.flatten.sort
-  }
+  combinators = Hash[patterns.map {|k,v| [k, v.flat_map {|p| combinators_by_pattern(p)}.sort]}.sort]
 
   puts "Counts:"
   pp Hash[combinators.map {|k,v| [k,v.size] }] 
@@ -28,7 +25,7 @@ end
 
 def find_patterns(max_n)
   patterns = Hash.new { |h, k| h[k] = [] }
-  patterns[1] = ["X"] # placeholder for variables
+  patterns[1] = ["X"] # in "patterns", X is a placeholder for variables
 
   # Alternate between search and simplify steps many times to build up the set of candidate patterns
   (max_n / 2 + 1).times do
@@ -75,9 +72,7 @@ def find_patterns(max_n)
   patterns.keep_if {|k, v| k <= max_n }
 
   # Filter out patterns that cannot be combinators (don't start with λ)
-  patterns.each_pair {|k, v| 
-    patterns[k] = v.keep_if {|p| p.start_with? "λ" }.sort
-  }
+  patterns = Hash[patterns.map {|k, v| [k, v.keep_if {|p| p.start_with? "λ"}]}]
 end
 
 def combinators_by_pattern(pattern)
